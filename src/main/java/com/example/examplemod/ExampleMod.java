d05916bab6cd8105ae5cc67c86561e1665a0cf8c
Kenneth Bell <kennethbe4@outlook.com>
2020-12-15 16:31:09 -0800

    Updated mcmod.info

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,6 +1,5 @@
 package com.example.examplemod;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
