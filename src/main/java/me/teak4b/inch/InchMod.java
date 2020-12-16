7aa2d2f762909a20bacd8c54e146ab6d0efef080
Kenneth Bell <kennethbe4@outlook.com>
2020-12-16 08:41:15 -0800

    Added InchMod

--- src/main/java/me/teak4b/inch/InchMod.java
+++ src/main/java/me/teak4b/inch/InchMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package me.teak4b.inch;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = InchMod.MODID, name = InchMod.NAME, version = InchMod.VERSION)
+public class InchMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "inchmod";
+    public static final String NAME = "Inch Client";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -27,6 +27,5 @@ public class ExampleMod
     {
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-        
     }
 }
